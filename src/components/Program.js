6e013773cc85cd9744572915441d0675563d8258
Frank J. Reashore <frank.reashore@gov.bc.ca>
2017-11-16 16:05:01 -0800

    Saving

--- src/components/Program.js
+++ src/components/Program.js
@@ -1,6 +1,23 @@
+
 import React, { Component } from 'react';
+// import '@progress/kendo-theme-default/dist/all.css';
+// import { Calendar } from '@progress/kendo-dateinputs-react-wrapper';
+// import { kendo } from "@progress/kendo-ui";
 
 class Program extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dateTime: new Date()
+    };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event) {
+    this.state({ dateTime: event.sender.value() });
+  }
+
   render() {
     return (
       <div className="panel panel-primary">
@@ -22,6 +39,10 @@ class Program extends Component {
               <input type="text" name="startDate" ref="_startDate" className="form-control" placeholder="Start Date" />
             </div>
 
+            {/* <div className="form-group">
+              <label htmlFor="startDate">Start Date:</label>
+              <Calendar value={this.state.dateTime} change={this.onChange} className="form-control" />
+            </div> */}
           </form>
         </div>
       </div>
