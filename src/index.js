57952e0b05fa259bc754faae74528b13ce06906d
Frank J. Reashore <frank.reashore@gov.bc.ca>
2017-11-24 17:01:35 -0800

    Added Redux functionality

--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import '../node_modules/bootstrap/dist/css/bootstrap-theme.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
 const reactRoot = document.getElementById('reactRoot');
-ReactDOM.render(<App />, reactRoot);
+
+const reducer = function() {
+  return {
+    junk: 42
+  };
+};
+const store = createStore(reducer);
+
+//ReactDOM.render(<App />, reactRoot);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  reactRoot);
 
 registerServiceWorker();
