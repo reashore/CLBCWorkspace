cf47334da212c431fde5e5d6d2e7c46812932d38
Frank J. Reashore <frank.reashore@gov.bc.ca>
2017-11-27 16:52:15 -0800

    Fixed file type

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './components/App.jsx';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
@@ -10,19 +10,20 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
 const reactRoot = document.getElementById('reactRoot');
 
-const reducer = function() {
+const reducer = function () {
   return {
     junk: 42
   };
 };
+
 const store = createStore(reducer);
 
-//ReactDOM.render(<App />, reactRoot);
-
-ReactDOM.render(
+const rootComponent =
   <Provider store={store}>
     <App />
-  </Provider>,
-  reactRoot);
+  </Provider>;
+
+//ReactDOM.render(<App />, reactRoot);
+ReactDOM.render(rootComponent, reactRoot);
 
 registerServiceWorker();
